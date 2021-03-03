c4db9f1d5b0e4c7fdfdbd8c7b3c7cdc29348d15e
IvanGM25 <ivan-gabriel@live.com.ar>
2021-03-03 19:45:29 -0300

    Se implemento la dependencia yargs

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
+const argv = require('yargs')
 
+.option('b',{
+    alias: 'base',
+    tipo: 'number',
+    demandOption: true,
+    describe:'Es la base de la tabla de multiplicar'
+})
+.option('l',{
+    alias: 'limite',
+    tipo: 'number',
+    demandOption: false,
+    default: 10,
+    describe:'Es el limite de la tabla de multiplicar'
+})
+.option('v',{
+    alias: 'visualizar',
+    tipo: 'boolean',
+    demandOption: false,
+    default: false,
+    describe:'EMuestra tabla en consola'
+})
+.check((argv,options)=>{
+
+    if(isNaN(argv.b)){
+        throw new Error('La base tiene que ser un numero');
+    }
+    return true
+
+
+})
+.help()
+.version()
+
+.argv;
 
 const {multiplicar} = require('./multiplicador');
 
-console.log(argv);
-
-const base = 5;
-
-multiplicar(base);
+multiplicar(argv.b,argv.l, argv.v);
